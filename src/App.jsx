a2bd1037e688f433c8efaec632ff9e82e9b91e6c
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-29 17:13:39 +0200

    completato con bonus colore diverso al bottone per il linguaggio  attualmente selezionato

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,41 @@
-import { useState } from 'react'
-import languages from './languages'
-import './App.css';
+import { useState } from "react";
+import languages from "./languages";
+import "./App.css";
 
 function App() {
   const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
   return (
     <>
-    <header className="page-header">
-      <h1>Learn Web development</h1>
-    </header>
+      <header className="page-header">
+        <h1>Learn Web development</h1>
+      </header>
 
-    <main className='container'>
+      <main className="container">
+        <div className="buttons-container">
+          {languages.map((language) => {
+            //stabilisco se questo bottone è quello attivo.
+            const isActive = language.id === selectedLanguage.id;
+            //Se 'isActive' è true, aggiungo la classe 'active', altrimenti no.
+            const buttonClassName = `btn ${isActive ? "active" : ""}`;
+            return (
+              <button
+                key={language.id}
+                className={buttonClassName}
+                onClick={() => setSelectedLanguage(language)}
+              >
+                {language.title}
+              </button>
+            );
+          })}
+        </div>
 
-      <div className='buttons-container'>
-
-      {languages.map((language) => {
-//stabilisco se questo bottone è quello attivo.
-          const isActive = language.id === selectedLanguage.id;
-//Se 'isActive' è true, aggiungo la classe 'active', altrimenti no.
-          const buttonClassName = `btn ${isActive ? 'active' : ''}`;
-          return(
-            <button
-            key={language.id}
-            className={buttonClassName}
-            onClick={() => setSelectedLanguage(language)}
-             >
-             {language.title}
-             </button>
-          )
-})}
-      </div>
-
-      <div className='card'>
-        <h2>{selectedLanguage.title}</h2>
-        <p>{selectedLanguage.description}</p>
-      </div>
-
-    </main>
-  </>
-  )
+        <div className="card">
+          <h2>{selectedLanguage.title}</h2>
+          <p>{selectedLanguage.description}</p>
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
