2ea5bcf59a080afc44665db437dc9991bbd8a1ee
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-29 15:31:09 +0200

    struttura jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { useState } from 'react'
+import languages from './languages'
+
 function App() {
+  const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
   return (
-    <>
+    <div className='container'>
+
+      <h1>Learn Web development</h1>
+
+      <div className='buttons-container'>
+
+      </div>
+
+      <div className='card'>
+
+      </div>
+
+    </div>
       
-    </>
   )
 }
 
